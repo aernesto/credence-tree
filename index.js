eadd4a04348939e231e33ed421370dac9dc58fa6
Nicholas Hanson-Holtry <nicholas.hanson.holtry@gmail.com>
2016-06-12 23:34:25 -0500

    added basic search page, switched to less

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 
-// dependencies
+// set-up dependencies
+
+var lessMiddleware = require('less-middleware');
+
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 var express = require('express');
-
-// set-up
-
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(lessMiddleware(__dirname + '/public')); // 1
+app.use(express.static(__dirname + '/public')); // 2
 
-// routing
+// routing information
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/home');
+});
+
+app.get('/basic-search', urlencodedParser, function(request, response) {
+  response.render('pages/basic-search', {query: request.param('query')});
 });
 
 // run the app
